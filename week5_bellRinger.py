850a1afbf6670ae5d1fe48cb317227fd19b4b96a
yjacobo2 <yjacobo2@cps.edu>
2025-11-06 18:47:13 +0000

    John F. Kennedy

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -30,6 +30,10 @@ reversed_speech = print (I_have_a_dream [::-1])
 # Problem Set 2: Extracting Information
 # From Descriptions:
 # Extract the name of the famous personality from the quote "Ask not what your country can do for you — ask what you can do for your country. - John F. Kennedy"
+famous_quote = "Ask not what your country can do for you - ask what you can do for your country. - John F. Kennedy"
+john_f_kennedy = print(famous_quote.find("John F. Kennedy"))
+# output: 83
+extracted_name = print(famous_quote[83:])
 
 # Manipulating Words:
 # Given the string info = "Python is fun. Fun is good. Good is subjective.",
